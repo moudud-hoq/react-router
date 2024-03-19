ecf3ea353a65aba0ade089072640cab1746b0bdf
moududulhaque <moududulhaque@gmail.com>
2024-03-19 19:28:15 +0600

    Nice Things Working like navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+  },
+  {
+    path: "/Contact",
+    element: <div>Call me right now</div>,
+  },
+  {
+    path: "/About",
+    element: <div>Hello world!2</div>,
   },
 ]);
 
